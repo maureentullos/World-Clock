4cfa64107516b03a68095cee9096ac8289bdff5f
maureentullos <Mswalsh220@gmail.com>
2023-08-19 19:09:01 -0400

    Added the functionality for select

--- javascript/index.js
+++ javascript/index.js
@@ -2,32 +2,51 @@
 function updateTime(){
 //Boston
 let bostonElement = document.querySelector("#boston");
-let bostonDateElement = bostonElement.querySelector(".date");
-let bostonTimeElement = bostonElement.querySelector(".time");
-let bostonTime = moment().tz("America/New_York");
+if (bostonElement)
+    {
+    let bostonDateElement = bostonElement.querySelector(".date");
+    let bostonTimeElement = bostonElement.querySelector(".time");
+    let bostonTime = moment().tz("America/New_York");
 
-bostonDateElement.innerHTML = bostonTime.format("MMMM Do, YYYY");
-bostonTimeElement.innerHTML = bostonTime.format("h:mm:ss [<small>]A[</small>]");
+    bostonDateElement.innerHTML = bostonTime.format("MMMM Do, YYYY");
+    bostonTimeElement.innerHTML = bostonTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 
 //Dublin
 let dublinElement = document.querySelector("#dublin");
-let dublinDateElement = dublinElement.querySelector(".date");
-let dublinTimeElement = dublinElement.querySelector(".time");
-let dublinTime = moment().tz("Europe/Dublin");
+    if (dublinElement)
+    {
+    let dublinDateElement = dublinElement.querySelector(".date");
+    let dublinTimeElement = dublinElement.querySelector(".time");
+    let dublinTime = moment().tz("Europe/Dublin");
 
-dublinDateElement.innerHTML = dublinTime.format("MMMM Do, YYYY");
-dublinTimeElement.innerHTML = dublinTime.format("h:mm:ss [<small>]A[</small>]");
+    dublinDateElement.innerHTML = dublinTime.format("MMMM Do, YYYY");
+    dublinTimeElement.innerHTML = dublinTime.format("h:mm:ss [<small>]A[</small>]");
+    };
 };
 
 updateTime();
 setInterval(updateTime, 1000);
 
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_" , " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+            <div>
+                <h2>${cityName}</h2>
+                <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format("h:mm:ss")}<small>PM</small>
+                </div>
+    </div>
+    `;
+}
 
-//Dublin
-let dublinElement = document.querySelector("#dublin");
-let dublinDateElement = dublinElement.querySelector(".date");
-let dublinTimeElement = dublinElement.querySelector(".time");
-let dublinTime = moment().tz("Europe/Dublin");
 
-dublinDateElement.innerHTML = dublinTime.format("MMMM Do, YYYY");
-dublinTimeElement.innerHTML = dublinTime.format("h:mm:ss [<small>]A[</small>]");
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
+
