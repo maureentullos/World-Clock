eb62b79e6dbec0584f0b8d8c90f4c4552d69c00e
maureentullos <Mswalsh220@gmail.com>
2023-08-19 19:18:34 -0400

    small changes

--- javascript/index.js
+++ javascript/index.js
@@ -39,7 +39,7 @@ function updateCity(event) {
                 <h2>${cityName}</h2>
                 <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
                 </div>
-                <div class="time">${cityTime.format("h:mm:ss")}<small>PM</small>
+                <div class="time">${cityTime.format("h:mm:ss ")}<small>${cityTime.format(" A")}</small>
                 </div>
     </div>
     `;
