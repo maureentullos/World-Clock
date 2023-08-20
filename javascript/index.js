6027116577d04fd9c97e8a764a5d3d07141ea123
maureentullos <Mswalsh220@gmail.com>
2023-08-19 20:03:22 -0400

    Added Tokyo to homepage

--- javascript/index.js
+++ javascript/index.js
@@ -23,7 +23,21 @@ let dublinElement = document.querySelector("#dublin");
     dublinDateElement.innerHTML = dublinTime.format("MMMM Do, YYYY");
     dublinTimeElement.innerHTML = dublinTime.format("h:mm:ss [<small>]A[</small>]");
     };
+//Tokyo
+    let tokyoElement = document.querySelector("#tokyo");
+    if (tokyoElement)
+    {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do, YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+    };
 };
+
+
+
 
 updateTime();
 setInterval(updateTime, 1000);
